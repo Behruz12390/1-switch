eac25d8e93a7f0990327d86e036a36c79b4c3356
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-07-20 12:02:27 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+let a = +prompt('Birdan yetigacha son yozing')
+switch(a){
+    case 1:
+        alert('Dushanba')
+        break;
+    case 2:
+        alert('Seshanba')
+        break;
+    case 3:
+        alert('Chorshanaba')
+        break;
+    case 4:
+        alert('Payshanba')
+        break;
+    case 5:
+        alert('Juma')
+        break;
+    case 6:
+        alert('Shanba')
+        break;
+    case 7:
+        alert('Yakshanba')
+        break;
+    default:
+        alert('Haftada 7ta kun')
+}
